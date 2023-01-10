7cce5ea7d18a057f91b242a53633bd392338901e
Bamjong <ebune123@naver.com>
2023-01-11 03:45:07 +0900

    ex01 138p

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -18,10 +19,17 @@ import lombok.extern.log4j.Log4j;
 
 public class SampleController {
 	
+//	TodoDTO에서 @DateTimeFormat(pattern = "yyyy/MM/dd") 을 설정해주었기 때문에 @InitBinder 작성을 안해주어도 됨.
+//	@InitBinder
+//	public void InitBinder(WebDataBinder binder) {
+//		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+//	}
+	
+	
 	@GetMapping("/basicOnlyGet")
 	public void basic() {
 		log.info("basic get..........");
-		
 	}
 	
 	@GetMapping("/ex01")
@@ -47,5 +55,11 @@ public class SampleController {
 	public String ex02Bean(SampleDTOList list) {
 		log.info("list dtos: "+ list);
 		return "ex02Bean";
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo: " + todo);
+		return "ex03";
 	}
 }
